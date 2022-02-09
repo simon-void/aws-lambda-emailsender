2c371889121be65d0cf4334d78a20bfc2349dc8b
stephans <simon.void+git@posteo.net>
2022-02-09 16:32:39 +0100

    update all the dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("jvm") version "1.3.50"
+	kotlin("jvm") version "1.6.10"
 	java
-	id("com.github.johnrengelman.shadow") version "5.1.0"
+	id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "de.gmx.simonvoid"
@@ -16,15 +16,15 @@ repositories {
 
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("com.amazonaws:aws-lambda-java-core:1.2.0")
-	implementation("com.amazonaws:aws-java-sdk-ses:1.11.656")
+	implementation("com.amazonaws:aws-lambda-java-core:1.2.1")
+	implementation("com.amazonaws:aws-java-sdk-ses:1.12.150")
 }
 
 tasks {
 	withType<KotlinCompile> {
 		kotlinOptions {
 			freeCompilerArgs = listOf("-Xjsr305=strict")
-			jvmTarget = "1.8"
+			jvmTarget = "11"
 		}
 	}
 
